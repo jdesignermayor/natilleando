ecfcd88897d335ec2e38b0e4d9897debaa35dd47
José Luis Pulgarín <jdesignermayor@gmail.com>
2022-01-18 17:53:05 -0500

    feat: se agrega la funcionalidad de resetear datos del usuario

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,53 +1,61 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { supabase } from '../supabaseclient';
+import React, { useContext, useState, useEffect } from "react";
+import { supabase } from "../supabaseclient";
 
 const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState();
-    const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
+  const resetUser = () => {
+    setUser();
+    setIsLoggedIn(false);
+    setIsLoading(false);
+  };
 
-    useEffect(() => {
-
-        /** 
+  useEffect(() => {
+    /** 
             @description Check active sessions and sets the user
         **/
-        const session = supabase.auth.session();
-        setUser(session?.user ?? null);
-        setIsLoading(false);
+    const session = supabase.auth.session();
+    setUser(session?.user ?? null);
+    setIsLoading(false);
 
-        /** 
-            @description  Listen for changes on auth state (logged in, signed out, etc.) 
-        **/
-        const { data: listener } = supabase.auth.onAuthStateChange(
-            async (event, session) => {
-                setUser(session?.user ?? null);
-                setIsLoading(false);
-            }
-        )
-
-        return () => {
-            listener?.unsubscribe();
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        if (session?.user) {
+          setUser(session?.user ?? null);
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
         }
-    }, []);
+        setIsLoading(false);
+      }
+    );
 
-    // Will be passed down to Signup, Login and Dashboard components
-    const value = {
-        signUp: (data) => supabase.auth.signUp(data),
-        signIn: (data) => supabase.auth.signIn(data),
-        signOut: () => supabase.auth.signOut(),
-        user,
-    }
+    return () => {
+      listener?.unsubscribe();
+    };
+  }, []);
 
-    return (
-        <AuthContext.Provider value={value}>
-            {!isLoading && children}
-        </AuthContext.Provider>
-    )
-}
+  // Will be passed down to Signup, Login and Dashboard components
+  const value = {
+    user,
+    isLoggedIn,
+    resetUser,
+    signUp: (data) => supabase.auth.signUp(data),
+    signIn: (data) => supabase.auth.signIn(data),
+    signOut: () => supabase.auth.signOut(),
+  };
 
+  return (
+    <AuthContext.Provider value={value}>
+      {!isLoading && children}
+    </AuthContext.Provider>
+  );
+};
 
 export function useAuth() {
-    return useContext(AuthContext)
+  return useContext(AuthContext);
 }
