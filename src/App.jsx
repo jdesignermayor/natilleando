2d936f6527885b433db690defc179e15f0f554f6
José Luis Pulgarín <jpulgarin@intelligencetv.co>
2022-02-09 10:53:31 -0500

    stupid changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,27 @@ import { RequestForm } from "./pages/submision/RequestForm";
 import { Login } from "./pages/login/Login";
 import { Dashboard } from "./pages/dashboard/Dashboard";
 import { NotFound } from "./pages/404/NotFound";
+import { Navigate, Outlet } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [auth, setAuth] = useState(null);
+
+  function PrivateOutlet() {
+    return auth ? <Outlet /> : <Navigate to="/login" />;
+  }
+
   return (
     <>
-    <Navbar />
+      <Navbar />
       <Provider store={store}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/form" element={<RequestForm />} />
+          <Route path="/dashboard" element={<PrivateOutlet />}>
+            <Route path="" element={<Dashboard />} />
+          </Route>
           <Route path="/login" element={<Login />} />
+          <Route path="/form" element={<RequestForm />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Provider>
---
+++ src/hooks/useAuth.js
@@ -0,0 +1,5 @@
+import { useSupabase } from "./useSupabase";
+
+export const useAuth = () => {
+
+}
